8740cda27e27d876cdbecde4c54809d8f901562a
Ahmed Mustafa <42010147.ahmed@acu.edu.eg>
2024-05-01 21:45:02 +0300

    Update host and port in main.py

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ async def get_recommendations(recommendation_request: RecommendationRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
